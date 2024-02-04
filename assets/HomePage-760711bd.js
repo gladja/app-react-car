331820bac26afe4fa5e1893539078bbbe2b20fe1
gladja <94562072+gladja@users.noreply.github.com>
2024-02-04 19:33:10 +0000

    Deploying to gh-pages from @ gladja/app-react-car@ba69a9b4a2b1f219fadb53568571802d1f48f0ed 🚀

---
+++ assets/HomePage-760711bd.js
@@ -0,0 +1,33 @@
+import{s as e,u as i,j as t}from"./index-02120bb4.js";import{B as s,C as a}from"./Container-f8e9ac4e.js";const r=e.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+`,o=e.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`,c=e.h1`
+  font-size: 48px;
+  font-weight: 700;
+  line-height: 1.4;
+  min-width: 300px;
+  max-width: 500px;
+`,l=e.span`
+  color: #3470ff;
+`,x=e.p`
+  font-size: 24px;
+  line-height: 1.5;
+  min-width: 200px;
+  max-width: 400px;
+  margin-bottom: 20px;
+`,d=e.div`
+  display: flex;
+  align-items: center;
+`,p=e.img`
+  max-width: 100%;
+  margin-left: 30%;
+`,h="/app-react-car/assets/car-0aa3a799.png",m=()=>{const n=i();return t.jsx(t.Fragment,{children:t.jsxs(r,{children:[t.jsxs(o,{children:[t.jsxs(c,{children:["Want to ",t.jsx(l,{children:"find the car"})," you want when you need it?"]}),t.jsx(x,{children:"Do you want to discover the best cars for you?"}),t.jsx(s,{size:"large",type:"primary",htmlType:"button",onClick:()=>n("/catalog"),style:{padding:"0 44px",borderRadius:"12px"},children:"Discover"})]}),t.jsx(d,{children:t.jsx(p,{src:h,alt:"car"})})]})})},u=()=>t.jsx(t.Fragment,{children:t.jsx(a,{children:t.jsx(m,{})})});export{u as default};
